74c73939ae06fd58bd32b7b9badfcafa1fa3120e
Martin Bernstorff <martinbernstorff@gmail.com>
2022-03-25 10:52:01 +0100

    Further polishing

--- tests/add_outcome_tests.py
+++ tests/add_outcome_tests.py
@@ -43,17 +43,22 @@ def test_multiple_citizens():
 
 def str_to_df(str):
     from io import StringIO
-    import pandas as pd
 
     df = pd.read_table(StringIO(str), sep=",", index_col=False)
 
-    convert_cols_with_matching_colnames_to_datetime(pd, df, "timestamp")
+    convert_cols_with_matching_colnames_to_datetime(df, "timestamp")
 
     # Drop "Unnamed" cols
     return df.loc[:, ~df.columns.str.contains("^Unnamed")]
 
 
-def convert_cols_with_matching_colnames_to_datetime(pd, df, colname_substr):
+def convert_cols_with_matching_colnames_to_datetime(df: DataFrame, colname_substr: str):
+    """Convert columns that contain colname_substr in their name to datetimes
+
+    Args:
+        df (DataFrame)
+        colname_substr (str): Substring to match on.
+    """
     df.loc[:, df.columns.str.contains(colname_substr)] = df.loc[
         :, df.columns.str.contains(colname_substr)
     ].apply(pd.to_datetime)
