b700fd441230ab824cb93fcf1f74724f0b9d9c8c
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-03-29 14:25:55 +0200

    clean up source

--- setup.py
+++ setup.py
@@ -2,19 +2,14 @@ from setuptools import setup
 from setuptools import find_packages
 import os
 
-with open("README.md", encoding="utf-8") as f:
-    long_description = f.read()
-
-with open("requirements.txt", encoding="utf-8") as f:
-    requirements = f.read().split("\n")
 
 path = os.path.join("src", "about.py")
 
 with open(path) as f:
     v = f.read()
-    for l in v.split("\n"):
-        if l.startswith("__version__"):
-            __version__ = l.split('"')[-2]
+    for line in v.split("\n"):
+        if line.startswith("__version__"):
+            __version__ = line.split('"')[-2]
 
 
 def setup_package():
