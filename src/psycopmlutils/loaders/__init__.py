c79c6ececb410d0c1b54691d735c40066df28d65
Martin Bernstorff <martinbernstorff@gmail.com>
2022-05-02 15:21:37 +0200

    Fix init

--- src/psycopmlutils/loaders/__init__.py
+++ src/psycopmlutils/loaders/__init__.py
@@ -1,1 +1,6 @@
+from .load_demographics import LoadDemographics
+from .load_diagnoses import LoadDiagnoses
+from .load_lab_results import LoadLabResults
+from .load_medications import LoadMedications
+from .load_visits import LoadVisits
 from .sql_load import sql_load
