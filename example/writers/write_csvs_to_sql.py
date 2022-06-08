f19f747f39c9689f68f34d019a32743b5d4f134e
Martin Bernstorff <ryqiem@gmail.com>
2022-06-08 14:03:22 +0200

    Add sql_writer

--- example/writers/write_csvs_to_sql.py
+++ example/writers/write_csvs_to_sql.py
@@ -2,18 +2,41 @@ from psycopmlutils.writers.sql_writer import write_df_to_sql
 from pathlib import Path
 import pymssql
 import pandas as pd
+from wasabi import Printer
 
 if __name__ == "__main__":
+    msg = Printer(timestamp=True)
+
     midtx_path = Path("\\\\tsclient\\X\\MANBER01\\documentLibrary")
 
     splits = ["test", "val", "train"]
 
     for dataset_name in splits:
+        rows_per_chunk = 2_000
         base_path = midtx_path / "feature_generation" / "toy_example"
 
+        msg.info(f"{dataset_name}: Reading features")
         feature_path = base_path / f"{dataset_name}_features.csv"
         df_features = pd.read_csv(feature_path)
-        write_df_to_sql(df=df_features, table_name=f"t2d_{dataset_name}_features", if_exists="replace")
 
-        # event_path = base_path / f"{dataset_name}_events.csv"
-        # df_events = pd.read_csv(event_path)
+        msg.info(f"{dataset_name}: Writing features")
+        write_df_to_sql(
+            df=df_features,
+            table_name=f"psycop_t2d_{dataset_name}_features",
+            if_exists="replace",
+            rows_per_chunk=rows_per_chunk,
+        )
+
+        msg.info(f"{dataset_name}: Reading events")
+        event_path = base_path / f"{dataset_name}_events.csv"
+        df_events = pd.read_csv(event_path)
+
+        msg.info(f"{dataset_name}: Writing events")
+        write_df_to_sql(
+            df=df_events,
+            table_name=f"psycop_t2d_{dataset_name}_events",
+            if_exists="replace",
+            rows_per_chunk=rows_per_chunk,
+        )
+
+        msg.good(f"{dataset_name}: Succesfully wrote {dataset_name} to sql")
--- src/psycopmlutils/writers/sql_writer.py
+++ src/psycopmlutils/writers/sql_writer.py
@@ -1,9 +1,41 @@
 import pandas as pd
 
+import urllib
+import urllib.parse
+
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+from tqdm import tqdm
+
+from wasabi import msg
+
+
+def chunker(seq, size):
+    # from http://stackoverflow.com/a/434328
+    return (seq[pos : pos + size] for pos in range(0, len(seq), size))
+
+
+def insert_with_progress(df: pd.DataFrame, table_name: str, conn, rows_per_chunk):
+    with tqdm(total=len(df)) as pbar:
+        for i, chunked_df in enumerate(chunker(df, rows_per_chunk)):
+            replace = "replace" if i == 0 else "append"
+
+            chunked_df.to_sql(
+                schema="fct",
+                con=conn,
+                name=table_name,
+                if_exists=replace,
+                index=False,
+            )
+
+            pbar.update(rows_per_chunk)
+
 
 def write_df_to_sql(
     df: pd.DataFrame,
     table_name: str,
+    rows_per_chunk: int,
     server: str = "BI-DPA-PROD",
     database: str = "USR_PS_Forsk",
     if_exists: str = "fail",
@@ -18,9 +50,17 @@ def write_df_to_sql(
         if_exists (str, optional): What to do if the table already exists. Takes {'fail’, 'replace’, 'append’}. Defaults to "fail".
     """
 
-    df.to_sql(
-        name=table_name,
-        con=f"mssql+pymssql://{server}/{database}",
-        if_exists=if_exists,
-        schema="fct",
+    driver = "ODBC Driver 13 for SQL Server"
+
+    params = urllib.parse.quote(
+        f"DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection=yes"
+    )
+
+    url = f"mssql+pyodbc:///?odbc_connect={params}"
+
+    engine = create_engine(url=url, poolclass=NullPool, fast_executemany=True)
+    conn = engine.connect()
+
+    insert_with_progress(
+        df=df, table_name=table_name, rows_per_chunk=rows_per_chunk, conn=conn
     )
