1ede690b242c7f49090ec296a1f25101be53b194
Martin Bernstorff <ryqiem@gmail.com>
2022-06-08 14:03:31 +0200

    Add sql_loader with examples

--- example/loaders/load_sql.py
+++ example/loaders/load_sql.py
@@ -1,6 +1,6 @@
 from psycopmlutils.loaders.sql_load import sql_load
 
 if __name__ == "__main__":
-    view = "[FOR_SFI_fritekst_resultat_udfoert_i_psykiatrien_aendret_2011]"
+    view = "[psycop_t2d_train_features]"
     sql = "SELECT * FROM [fct]." + view
-    df = sql_load(sql, chunksize = None)
+    df = sql_load(sql, chunksize=None, format_timestamp_cols_to_datetime=False)
