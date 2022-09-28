b2ec7dbe3bcfc706bc7372fd64d6776f10ddbccf
bokajgd <bokajgd@gmail.com>
2022-09-28 10:53:20 +0200

    Handle duplicate rows in coercion data Fixes #11

--- src/psycop_feature_generation/loaders/raw/load_coercion.py
+++ src/psycop_feature_generation/loaders/raw/load_coercion.py
@@ -27,9 +27,12 @@ def coercion_duration(
     Returns:
         pd.DataFrame
     """
+    coercion_discard = """('Døraflåsning', 'Personlig afskærmning over 24 timer', 'Koordinationsplan', 
+    'Udskrivningsaftale', 'Særlige dørlåse', 'Personlige alarm- og pejlesystemer', 'Andet' )"""
+
     view = "[FOR_tvang_alt_hele_kohorten_inkl_2021]"
 
-    sql = f"SELECT dw_ek_borger, datotid_start_sei, varighed_timer_sei FROM [fct].{view} WHERE datotid_start_sei IS NOT NULL"
+    sql = f"SELECT dw_ek_borger, datotid_start_sei, varighed_timer_sei, typetekst_sei FROM [fct].{view} WHERE datotid_start_sei IS NOT NULL AND typetekst_sei NOT IN {coercion_discard}"
 
     if coercion_type and reason_for_coercion is None:
 
@@ -44,6 +47,9 @@ def coercion_duration(
         sql += f" AND typetekst_sei = '{coercion_type}' AND begrundtekst_sei = '{reason_for_coercion}'"
 
     df = sql_load(sql, database="USR_PS_FORSK", chunksize=None, n_rows=n_rows)
+
+    # Drop duplicate rows
+    df = df.drop_duplicates(keep="first")
 
     df.rename(
         columns={"datotid_start_sei": "timestamp", "varighed_timer_sei": "value"},
