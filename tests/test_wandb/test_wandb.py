8f35bb21347436841c07562815ad75a44c7b8e91
Lasse <lasseh0310@gmail.com>
2022-10-04 14:09:28 +0200

    chore: remove unused test

--- tests/test_wandb/test_wandb.py
+++
@@ -1,8 +0,0 @@
-"""Test wandb logging."""
-from application.t2d.generate_features_and_write_to_disk import log_to_wandb
-
-# pylint: disable=missing-function-docstring
-
-
-def test_log_to_wandb():
-    log_to_wandb("test", "x", "y")
