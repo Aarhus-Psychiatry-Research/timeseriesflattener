afadaef6dc10628c7ee74fbf949ceb1457d89694
Lasse <lasseh0310@gmail.com>
2022-12-13 11:33:47 +0100

    docs: extended docs for autogenerated docstrings after review

--- src/timeseriesflattener/feature_spec_objects.py
+++ src/timeseriesflattener/feature_spec_objects.py
@@ -97,6 +97,12 @@ class BaseModel(PydanticBaseModel):
     # The docstring generator uses the `short_description` attribute of the `Doc`
     # class to generate the top of the docstring.
     # If you want to modify a docstring, modify the `short_description` attribute.
+    # Then, when you run tests, new docstrings will be generated which you can copy/paste into 
+    # the relevant files. This is necessary because 
+    # 1) we have inheritance and don't want to have one source of truth for docs
+    # 2) pylance reads the docstring directly from the static file
+    # This means we want to auto-generate docstrings to support the inheritance,
+    # but also need to hard-code the docstring to support pylance.
     class Doc:
         short_description: str = """Modified Pydantic BaseModel to allow arbitrary 
         types and disallow attributes not in the class."""
