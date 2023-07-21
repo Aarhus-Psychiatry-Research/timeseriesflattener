bd58c4dce341d1acddd59ee958027ce755c1b934
Lasse <lasseh0310@gmail.com>
2023-07-21 15:12:34 +0200

    fix: update name of column in synth text

--- src/timeseriesflattener/tests/test_timeseriesflattener/test_embedding_functions.py
+++ src/timeseriesflattener/tests/test_timeseriesflattener/test_embedding_functions.py
@@ -11,18 +11,18 @@ from timeseriesflattener.text_embedding_functions import (
 
 def test_embedding_fn(synth_text_data: pd.DataFrame):
     """Test that synth embedding function works as expected"""
-    df = synth_text_data.dropna(subset="text")
-    embedding_df = bow_test_embedding(df["text"])
+    df = synth_text_data.dropna(subset="value")
+    embedding_df = bow_test_embedding(df["value"])
     assert embedding_df.shape == (df.shape[0], 10)
 
 
 @pytest.mark.huggingface()
 def test_huggingface_embedding(synth_text_data: pd.DataFrame):
     """Test that the huggingface embedding function works as expected"""
-    df = synth_text_data.dropna(subset="text")
+    df = synth_text_data.dropna(subset="value")
     df = df.head(5)
     embedding_df = huggingface_embedding(
-        df["text"],
+        df["value"],
         model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
     )
     assert embedding_df.shape == (df.shape[0], 384)
@@ -31,10 +31,10 @@ def test_huggingface_embedding(synth_text_data: pd.DataFrame):
 @pytest.mark.huggingface()
 def test_sentence_transformer_embedding(synth_text_data: pd.DataFrame):
     """Test that the sentence-transformer embedding function works as expected"""
-    df = synth_text_data.dropna(subset="text")
+    df = synth_text_data.dropna(subset="value")
     df = df.head(5)
     embedding_df = sentence_transformers_embedding(
-        df["text"],
+        df["value"],
         model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
     )
     assert embedding_df.shape == (df.shape[0], 384)
