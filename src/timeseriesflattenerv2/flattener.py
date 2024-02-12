28518e0cf6a4b248652014ac5994dc227ffef275
Martin Bernstorff <martinbernstorff@gmail.com>
2024-02-12 12:07:18 +0000

    feat(#379): check that no value columns have the same name
    Fixes #379

--- src/timeseriesflattenerv2/flattener.py
+++ src/timeseriesflattenerv2/flattener.py
@@ -153,12 +153,44 @@ def _process_spec(
     )
 
 
+@dataclass(frozen=True)
+class SpecError(Exception):
+    description: str
+
+
+def _specs_are_without_conflicts(specs: Sequence[ValueSpecification]) -> Iter[SpecError]:
+    conflicting_value_col_names = (
+        Iter(specs)
+        .map(lambda s: s.value_frame.value_col_name)
+        .groupby(lambda value_col_name: value_col_name)
+        .filter(lambda val: len(val[1]) > 1)
+        .map(
+            lambda error: SpecError(
+                description=f"{error[0]} occurs in {len(error[1])} specs. All input value column names must be unique to avoid conflicts in the output."
+            )
+        )
+    )
+
+    return conflicting_value_col_names
+
+
 @dataclass
 class Flattener:
     predictiontime_frame: PredictionTimeFrame
     lazy: bool = True
 
     def aggregate_timeseries(self, specs: Sequence[ValueSpecification]) -> AggregatedFrame:
+        # Check for conflicts in the specs
+        conflicting_specs = _specs_are_without_conflicts(specs)
+
+        if conflicting_specs.count() > 0:
+            raise SpecError(
+                "Conflicting specs."
+                + "".join(
+                    conflicting_specs.map(lambda error: f"  \n - {error.description}").to_list()
+                )
+            )
+
         if not self.lazy:
             self.predictiontime_frame.df = self.predictiontime_frame.collect()  # type: ignore
             for spec in specs:
