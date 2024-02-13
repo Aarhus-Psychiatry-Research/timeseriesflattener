fdd6da0182357e86d39efea13a982118dd233d62
Martin Bernstorff <martinbernstorff@gmail.com>
2024-02-13 10:32:26 +0000

    ci: speed up benchmarks

--- src/conftest.py
+++ src/conftest.py
@@ -9,29 +9,6 @@ from timeseriesflattener.testing.load_synth_data import (
 from timeseriesflattener.testing.utils_for_testing import load_long_df_with_multiple_values
 
 
-def pytest_addoption(parser):  # noqa
-    parser.addoption(
-        "--skiphuggingface",
-        action="store_true",
-        default=False,
-        help="run tests that use huggingface models",
-    )
-
-
-def pytest_configure(config):  # noqa
-    config.addinivalue_line("markers", "huggingface: mark test as using huggingface models")
-
-
-def pytest_collection_modifyitems(config, items):  # noqa
-    if config.getoption("--skiphuggingface"):
-        # --skiphuggingface given in cli: skip huggingface tests
-        skip_hf = pytest.mark.skip(reason="remove --skiphuggingface option to run")
-        for item in items:
-            if "huggingface" in item.keywords:
-                item.add_marker(skip_hf)
-        return
-
-
 @pytest.fixture()
 def synth_prediction_times() -> DataFrame:
     """Load the prediction times."""
