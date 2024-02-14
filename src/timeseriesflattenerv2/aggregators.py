3bcadc05c26c4119ccb54f9c47e0e6cbc2b9272b
Martin Bernstorff <martinbernstorff@gmail.com>
2024-02-14 11:44:04 +0000

    update aggregators.py

--- src/timeseriesflattenerv2/aggregators.py
+++ src/timeseriesflattenerv2/aggregators.py
@@ -3,13 +3,25 @@ import polars as pl
 from timeseriesflattenerv2.feature_specs import Aggregator
 
 
-class MeanAggregator(Aggregator):
+class MinAggregator(Aggregator):
     def __call__(self, column_name: str) -> pl.Expr:
-        value_col_name = f"{column_name}_mean"
-        return pl.col(column_name).mean().alias(value_col_name)
+        value_col_name = f"{column_name}_min"
+        return pl.col(column_name).min().alias(value_col_name)
 
 
 class MaxAggregator(Aggregator):
     def __call__(self, column_name: str) -> pl.Expr:
         value_col_name = f"{column_name}_max"
         return pl.col(column_name).max().alias(value_col_name)
+
+
+class MeanAggregator(Aggregator):
+    def __call__(self, column_name: str) -> pl.Expr:
+        value_col_name = f"{column_name}_mean"
+        return pl.col(column_name).mean().alias(value_col_name)
+
+
+class CountAggregator(Aggregator):
+    def __call__(self, column_name: str) -> pl.Expr:
+        value_col_name = f"{column_name}_count"
+        return pl.col(column_name).count().alias(value_col_name)
