d317a64b00f0d2089bd40bd09095119f20a7b6c0
Martin Bernstorff <martinbernstorff@gmail.com>
2024-08-30 14:07:14 +0200

    update devcontainer.json and post-start.sh

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,10 +1,2 @@
-# Setup lefthook
-git init
-lefthook install
-
-# Disable Graphite pager
-gt user pager --disable
-
-# Install dependencies
-pip install -e ".[dev, test]"
-
+#!/bin/bash
+uv sync --frozen --all-extras
